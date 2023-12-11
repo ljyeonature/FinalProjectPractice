a08653f61267c33f970a92389111b9ab22e076c9
ljy131 <ljyeon131@gmail.com>
2023-12-11 17:03:44 +0900

    주석 풀기

--- demo/src/main/java/com/test/test_LJY.java
+++ demo/src/main/java/com/test/test_LJY.java
@@ -2,8 +2,8 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 
-// public class test_LJY {
-//     public static void main(String[] args) {
-// 		SpringApplication.run(DemoApplication.class, args);
-// 	}
-// }
+public class test_LJY {
+    public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
