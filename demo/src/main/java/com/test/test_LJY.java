8441f6fe1b5d289b8639aeff09d5f8c813405fbe
ljy131 <ljyeon131@gmail.com>
2023-12-11 16:36:09 +0900

    LJY_1211

---
+++ demo/src/main/java/com/test/test_LJY.java
@@ -0,0 +1,9 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+
+public class test_LJY {
+    public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
