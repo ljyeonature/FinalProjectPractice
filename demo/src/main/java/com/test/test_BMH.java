58e6fbf4576e056d740d6d9eff01eab51a7e2967
ljyeonature <ljyeon131@gmail.com>
2023-12-11 16:11:07 +0900

    LJY

--- demo/src/main/java/com/test/test_BMH.java
+++ demo/src/main/java/com/test/test_BMH.java
@@ -5,5 +5,6 @@ import org.springframework.boot.SpringApplication;
 public class test_BMH {
     public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		// 화이팅!!
 	}
 }
